9a70e1be4a9daf5b083c53587458a013b640c9b0
Mateusz Stobiecki <kyrusxx@icloud.com>
2021-02-10 19:26:12 +0100

    New content 10.02.2021

---
+++ main.js
@@ -0,0 +1,5 @@
+const name = 'Mateusz';
+const age = 22; 
+
+console.log(`nazywam się ${name} i mam ${age} lata.`);
+console.log(`Siemanko! Jestem ${name} i strona internetowa pwostała w wywniku szkolenia WTF. :)`);
