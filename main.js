4c9d81de89473181e2232fc29fd4bfc0ebb77c11
Mateusz Stobiecki <kyrusxx@icloud.com>
2021-02-14 22:34:23 +0100

    new content - 14.02.2021

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const age = 22;
 
 console.log(`nazywam się ${name} i mam ${age} lata.`);
 console.log(`Siemanko! Jestem ${name} i strona internetowa pwostała w wywniku szkolenia WTF. :)`);
+
+const heading = document.querySelector('.main-heading--js');
+heading.innerHTML = `Nazywam się ${name} i mam ${age} lata.`;
